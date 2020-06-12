af87296e545e9d48803e918e792e3fc364974869
Darpan <darpanagarwal1999@gmail.com>
2020-06-12 23:24:37 +0530

    first_commit

---
+++ date.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+
+
+exports.getDate = function () {
+     const today = new Date();
+     const options = {
+          weekday: "long",
+          day: "numeric",
+          month: "long"
+     };
+     let day = today.toLocaleDateString("en-US", options);
+     return day;
+}
+exports.getDay = function () {
+     const today = new Date();
+     const options = {
+          weekday: "long"
+     };
+
+
+
+     return today.toLocaleDateString("en-US", options);
+}
